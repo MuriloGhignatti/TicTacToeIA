9e145ff8120cb07fafb78e66246f7d517b6f1c32
joaokrol <joaogkrol@gmail.com>
2021-05-10 22:28:27 -0300

    Changed grid to 15 from 7

--- src/main/java/murilo/ghignatti/Main.java
+++ src/main/java/murilo/ghignatti/Main.java
@@ -8,7 +8,7 @@ public class Main{
 
     public static void main(String[] args) throws VertexAlreadyExistsException, NoSuchVertexException, AdjacencyAlreadyExistsException {
 
-        TicTacToe ticTacToe = new TicTacToe(7);
+        TicTacToe ticTacToe = new TicTacToe(15);
 
         ticTacToe.startInterface();
 
